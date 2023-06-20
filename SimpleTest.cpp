4a5d4e083e4ef7421590d95b233afce41f18ecf7
ilay-chen <ilaychen@gmail.com>
2023-06-20 03:34:27 +0300

    add some comments

--- SimpleTest.cpp
+++ SimpleTest.cpp
@@ -5,6 +5,10 @@
 
 int main(int argc, char **argv) {
 
+  // I try to test less iteration to debug. until i = 9 all good. if the loop is until 11
+  // from some reason reading from 8 it curropted... by the way, 9 and 10 is ok also 8 not.
+  // probably some memory violation... need to double check we dont write to wired palaces.
+  // also - are we initilaize new frames when we find?! (on max_frame_id) i dont think so..
 
     VMinitialize();
 //    for (uint64_t i = 0; i < (2 * NUM_FRAMES); ++i) {
