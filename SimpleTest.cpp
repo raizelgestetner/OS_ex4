f8eb157b10788a2d82086c4877f2fd221015c7ab
ilay-chen <ilaychen@gmail.com>
2023-06-21 12:11:34 +0300

    start point, simple and yaara test work

--- SimpleTest.cpp
+++ SimpleTest.cpp
@@ -11,19 +11,20 @@ int main(int argc, char **argv) {
   // also - are we initilaize new frames when we find?! (on max_frame_id) i dont think so..
 
     VMinitialize();
-//    for (uint64_t i = 0; i < (2 * NUM_FRAMES); ++i) {
-    for (uint64_t i = 0; i < 11; ++i) {
+    for (uint64_t i = 0; i < (2 * NUM_FRAMES); ++i) {
+//    for (uint64_t i = 0; i < 11; ++i) {
         printf("writing to %llu\n", (long long int) i);
-        printf("virtual addres %llu\n", (long long int) 5 * i * PAGE_SIZE);
+//        printf("virtual addres %llu\n", (long long int) 5 * i * PAGE_SIZE);
         VMwrite(5 * i * PAGE_SIZE, i);
     }
 
-//    for (uint64_t i = 0; i < (2 * NUM_FRAMES); ++i) {
-    for (uint64_t i = 0; i < 11; ++i) {
+    for (uint64_t i = 0; i < (2 * NUM_FRAMES); ++i) {
+//    for (uint64_t i = 0; i < 11; ++i) {
         word_t value;
-        printf("reading from %llu %d\n", (long long int) i, value);
-        printf("virtual addres %llu\n", (long long int) 5 * i * PAGE_SIZE);
+
+//        printf("virtual addres %llu\n", (long long int) 5 * i * PAGE_SIZE);
         VMread(5 * i * PAGE_SIZE, &value);
+        printf("reading from %llu %d\n", (long long int) i, value);
         assert(uint64_t(value) == i);
     }
     printf("success\n");
